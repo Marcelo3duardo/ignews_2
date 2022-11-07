227a90e036a29edd25b476a1bc468c10fc302d26
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2022-11-07 17:42:09 -0300

    Next-Auth_github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import '../styles/global.scss'
 import { AppProps } from 'next/app'
 import { Header } from '../Components/Header'
 
+import { getProviders as NextAuthProvider  } from 'next-auth/react'
+import { SessionProvider } from "next-auth/react"
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider >
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
