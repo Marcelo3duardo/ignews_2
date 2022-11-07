34b938310fc27cff09b1b94a1d9e0afb08315bf3
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2022-11-07 17:41:41 -0300

    NExtAuth_Github

--- src/Components/SingInButton/index.tsx
+++ src/Components/SingInButton/index.tsx
@@ -1,32 +1,34 @@
-import {FaGithub} from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss'
 import { FiX } from "react-icons/fi";
+import { signIn,signOut,useSession } from "next-auth/react"
 
 
-
-export function SingInButton(){
-    const IsUserLoggedIn = true;
-    return IsUserLoggedIn ? (
-        <button 
-        type="button" 
-        className={styles.SingInButton}
-        
-        > 
-        <FaGithub color="#04d361" />
-        Marcelo
-        <FiX color="#737380"  className={styles.closeIcon}/>
+export function SingInButton() {
+    const {data:session} =useSession()
+    
+    console.log(session)
+    return session ? (
+        <button
+            type="button"
+            className={styles.SingInButton}
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
-        
-    ):(
-        <button 
-        type="button" 
-        className={styles.SingInButton}
-        
-        > 
-        <FaGithub color="#eba417" />
-        Sing in GitHub
+
+    ) : (
+        <button
+            type="button"
+            className={styles.SingInButton}
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417" />
+            Sing in GitHub
         </button>
-        
+
     );
 
 }
