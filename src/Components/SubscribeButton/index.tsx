6a90e6e2f2ed4e69996df35294bd594a74403921
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2022-11-08 17:33:56 -0300

    erro can not read data Stripe

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../Services/api";
+import { getStripeJs } from "../../Services/stripe-js";
 import styles from "./styles.module.scss"
 
 interface subscribeButtonProps {
@@ -6,14 +8,35 @@ interface subscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: subscribeButtonProps) {
-    const {data:session} = useSession();
+    
+    //const [session] = useSession();
+    const { data: session } = useSession();
 
-    function handleSubscribe() {
-        if(!session){
+    async function handleSubscribe() {
+        if (!session) {
             signIn('github')
             return;
         }
+
+        try {
+            const response = await api.post('/subscribe')
+            /*
+            const response = await api.post('/subscribe', {
+                user: session
+            })*/
+            
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId: sessionId } )
+        } catch (err) {
+            alert(err.message);
+        }
     }
+
+
 
     return (
 
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,36 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { env } from "process";
+
 import { stripe } from "../../Services/stripe";
-import { getSession } from 'next-auth/react'
+import { getSession, useSession } from 'next-auth/react'
 
-export default async(req: NextApiRequest,res:NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
 
-    if (req.method == 'POST'){
+    if (req.method === 'POST') {
         const session = await getSession({req})
         
-        const stripecumstumer = await stripe.customers.create({
+        //const { data: session } = useSession()
+        //const user = await req.body.user;
+
+        const stripeCustomer = await stripe.customers.create({
             email: session.user.email,
             //metadata
         })
 
-        session.user
 
-        const stripeCheckoultSession = await stripe.checkout.sessions.create({
-            customer:stripecumstumer.id,
+        //session.user
+
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
-            line_items:[
-                {price: 'price_1Kfl3yFGYdwyE1tHH2tnLErf',quantity: 1}
+            line_items: [
+                { price: 'price_1Kfl3yFGYdwyE1tHH2tnLErf', quantity: 1 }
             ],
             mode: 'subscription',
-            allow_promotion_codes:true,
-            success_url: process.env.STRIPI_SUCCESS_URL,
-            cancel_url: process.env.STRIPI_CANCEL_URL,
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL,
         })
 
-        return res.status(200).json({sessionId: stripeCheckoultSession.id})
-    } else{
-        res.setHeader('Allow','POST')
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+    } else {
+        res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
     }
 }
